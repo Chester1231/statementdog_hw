47958475a3a70bdbc8665f8a133e082a8bfdd356
Chester <chestertang0214@gmail.com>
2022-06-04 18:04:35 +0800

    Fix: delete devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_113322) do
+ActiveRecord::Schema.define(version: 2022_06_04_100058) do
 
   create_table "portfolio_stocks", force: :cascade do |t|
     t.integer "portfolio_id"
@@ -41,18 +41,6 @@ ActiveRecord::Schema.define(version: 2022_05_23_113322) do
     t.index ["deleted_at"], name: "index_stocks_on_deleted_at"
     t.index ["market_type"], name: "index_stocks_on_market_type"
     t.index ["ticker"], name: "index_stocks_on_ticker", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
